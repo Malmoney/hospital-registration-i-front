892e7ecbeda2549c5b5d69ca1420aa565dbda776
qiaoba <qiaoba@dtstack.com>
2021-02-17 12:41:37 +0800

    feat: feat homePage_demo

---
+++ front_dev/src/components/hospital.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="hospital">
+    <div class="hospital_mian">
+      <div class="hospital_mian_coverimg">
+        <slot name="hospital_mian_coverimg"></slot>
+      </div>
+      <div class="hospital_mian_right">
+        <div class="hospitalName">
+          <slot name="hospitalName"></slot>
+        </div>
+        <div class="hospitalInfo">
+          <div class="hospitalInfo_info_level">
+            <slot name="hospitalInfo_info_level"></slot>
+            <div class="hospitalInfo_info_kind">
+              <slot name="hospitalInfo_info_kind"></slot>
+            </div>
+          </div>
+          <div class="hospitalInfo_info_distance">
+            <slot name="hospitalInfo_info_distance"></slot>
+          </div>
+        </div>
+        <div class="hospitalAddr">
+          <slot name="hospitalAddr"></slot>
+        </div>
+      </div>
+    </div>
+    <div class="hospital_foot"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.hospital {
+  width: 95%;
+  height: 150px;
+  background-color: #ffffff;
+  margin: auto;
+  margin-bottom: 10px;
+  border-radius: 8px;
+}
+.hospital_mian {
+  width: 100%;
+  height: 130px;
+  padding: 10px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+}
+.hospital_mian_coverimg {
+  height: 100%;
+  width: 140px;
+  background-color: red;
+}
+.hospital_mian_right {
+  height: 100%;
+  width: calc(100% - 150px);
+}
+.hospital_mian_right div {
+  height: calc(110px / 3);
+  line-height: calc(110px / 3);
+}
+.hospitalName {
+  text-align: left;
+  font-size: 1.1em;
+}
+.hospitalInfo {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.hospitalInfo_info_level {
+  font-size: 0.9em;
+  display: flex;
+  color: #e97b24;
+}
+.hospitalInfo_info_kind {
+  font-size: 0.8em;
+  margin-left: 5px;
+  color: #6b7aac;
+  width: 60px;
+}
+.hospitalAddr {
+  text-align: left;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-size: 0.9em;
+}
+.hospital_foot {
+  width: 100%;
+  height: 20px;
+}
+</style>
---
+++ front_dev/src/views/Home.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="home">
+    <div class="home_search"></div>
+    <div class="home_head">
+      <div class="home_head_title">诊前·诊中·诊后 全流程服务</div>
+      <div class="home_head_content">
+        <div
+          v-for="(item, index) in 13"
+          :key="index"
+          class="home_head_content_item"
+        >
+          item
+        </div>
+      </div>
+    </div>
+    <div class="home_hospital_list">
+      <HospitalList
+        class="home_hospital_list_item"
+        v-for="(item, index) in 4"
+        :key="index"
+      >
+        <template v-slot:hospitalName>XXX医院</template>
+        <template v-slot:hospitalInfo_info_level>三甲</template>
+        <template v-slot:hospitalInfo_info_kind>综合医院</template>
+        <template v-slot:hospitalInfo_info_distance>3.3km</template>
+        <template v-slot:hospitalAddr
+          >地址：山西省太原市小店区昌盛街51号银河公寓6号楼1单元602</template
+        >
+      </HospitalList>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HospitalList from "../components/hospital";
+
+export default {
+  name: "Home",
+  components: {
+    HospitalList
+  }
+};
+</script>
+<style>
+.home {
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #f3f3f3;
+}
+.home_search {
+  width: 100vw;
+  height: 40px;
+  background-color: yellow;
+}
+.home_head {
+  width: 100vw;
+  background-color: #ffffff;
+}
+.home_head_title {
+  display: flex;
+  align-items: center;
+  height: 40px;
+  width: 100%;
+  font-size: 1.2em;
+  text-align: left;
+  padding-left: 10px;
+  box-sizing: border-box;
+  font-weight: 600;
+}
+.home_head_content {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.home_head_content_item {
+  width: 20%;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.home_hospital_list {
+  width: 100vw;
+  margin-top: 20px;
+}
+</style>
